f5ad537dcf9972de2cb2d045a1359851643a68a8
ankurpatel1 <ankur.patel.0101@gmail.com>
2020-03-03 11:32:36 +0530

    added

---
+++ Runtime/FrameWork/Utility/ExtensionUtil.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Random = System.Random;
+
+namespace Utility
+{
+    public static class CollectionExtension
+    {
+        private static Random rng = new Random();
+
+        public static T RandomElement<T>(this IList<T> list)
+        {
+            return list[rng.Next(list.Count)];
+        }
+
+        public static T RandomElement<T>(this T[] array)
+        {
+            return array[rng.Next(array.Length)];
+        }
+    }
+}
+
---
+++ Runtime/FrameWork/Utility/Utility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace Utility
+{
+    [Serializable]
+    public class MinMax
+    {
+        [XmlElement(ElementName = "Min")]
+        public float Min;
+
+        [XmlElement(ElementName = "Max")]
+        public float Max;
+
+        public MinMax()
+        {
+        }
+
+        public MinMax(float inMin, float inMax)
+        {
+            Max = inMax;
+            Min = inMin;
+        }
+
+        public float GetRandomValue()
+        {
+            return UnityEngine.Random.Range(Min * 1000.0f, Max * 1000.0f) / 1000.0f;
+        }
+
+        public int GetRandomInt()
+        {
+            return (int)Math.Round(GetRandomValue(), MidpointRounding.AwayFromZero);
+        }
+
+        public bool IsInRange(float inValue)
+        {
+            if (inValue < Min || inValue > Max)
+                return false;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return ("Min : " + Min + ", Max : " + Max);
+        }
+    }
+}
